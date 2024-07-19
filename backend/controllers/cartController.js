ebbdebb5e4f81823a56e5799e09701d074104455
DidinDinesh <155463888+DidinDinesh@users.noreply.github.com>
2024-07-20 00:28:33 +0530

    small fixes

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -7,7 +7,7 @@ const addToCart = async (req, res) => {
     try {
         let userData = await userModel.findOne({_id:req.body.userId});
         let cartData = await userData.cartData || {};
-
+    
         if (!cartData[req.body.itemId]) 
         {
             cartData[req.body.itemId] = 1;
@@ -16,10 +16,11 @@ const addToCart = async (req, res) => {
             cartData[req.body.itemId] += 1;
         }
         await userModel.findByIdAndUpdate(req.body.userId, {cartData});
-        res.json({success:true, message:"Item added to cart"});
+        res.json({success:true, message:"Item Added to cart"})
+        
     } catch (error) {
         console.log(error);
-
+    
         res.json({success:false, message:"Error"});
     }
 }
